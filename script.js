02027a710d6a090820adf0cfe69ac75e54fb64cf
Pyr0 <121192159+Pyr0dv@users.noreply.github.com>
2023-07-17 18:09:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var pass = prompt("Password:")
 console.log(pass)
 var body = document.querySelector('.body')
-var appPass = "080511"
+var appPass = "820511"
 body.style.display = 'none';
 if(pass !== appPass){
     body.style.display = 'none';
